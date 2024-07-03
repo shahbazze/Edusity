d81f1674d453e81fa5dec767be7e928ab736b222
shahbazze <148189830+shahbazze@users.noreply.github.com>
2024-07-03 23:48:18 +0500

    Update VideoPlayer.jsx

--- src/components/Video player/VideoPlayer.jsx
+++ src/components/Video player/VideoPlayer.jsx
@@ -1,19 +1,25 @@
-import React, { useRef } from 'react'
-import "./Videoplayer.css"
-import college_video from "../../assets/edusity_assets/college.mp4"
-const VideoPlayer = ({playstate,setplaystate}) => {
-  const player=useRef(null);
-  const closedplayer=()=>{
-    if(e.target===player.current)
-      {
-        setplaystate(false);
-      }
-  }
-  return (
-    <div className={`Video-player hide ${playstate ?'':'hide'}`} ref={player} onClick={closedplayer}>
-        <video src={college_video} autoPlay muted controls></video>
-    </div>
-  )
-}
+import React, { useRef } from 'react';
+import "./Videoplayer.css";
+import college_video from "../../assets/edusity_assets/college.mp4";
 
-export default VideoPlayer
+const VideoPlayer = ({ playstate, setplaystate }) => {
+  const player = useRef(null);
+
+  const closedplayer = (e) => {
+    if (e.target === player.current) {
+      setplaystate(false);
+    }
+  };
+
+  return (
+    <div
+      className={`Video-player ${playstate ? '' : 'hide'}`}
+      ref={player}
+      onClick={closedplayer}
+    >
+      <video src={college_video} autoPlay muted controls></video>
+    </div>
+  );
+};
+
+export default VideoPlayer;
